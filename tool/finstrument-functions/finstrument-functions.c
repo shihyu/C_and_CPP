e06b90e3ad64f12169e488e8b918913ba30f7ce0
jason_yao <jason_yao@htc.com>
2014-04-22 00:23:08 +0800

    update

---
+++ tool/finstrument-functions/finstrument-functions.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+// gcc -finstrument-functions -finstrument-functions finstrument-functions.c -o finstrument-functions
+
+#define DUMP(func, call) printf("%s: func = %p, called by = %p\n", __FUNCTION__, func, call)
+
+void __attribute__((__no_instrument_function__))
+__cyg_profile_func_enter(void *this_func, void *call_site) {
+    DUMP(this_func, call_site);
+}
+void __attribute__((__no_instrument_function__))
+__cyg_profile_func_exit(void *this_func, void *call_site) {
+    DUMP(this_func, call_site);
+}
+
+void myfun(){
+
+
+}
+
+int main() {
+    puts(" Hello World! ");
+    myfun();
+    return  0 ;
+}
+
