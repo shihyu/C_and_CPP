6b2cbecfb2a11aa9fdf3e634e6d460495cfef5c6
jason_yao <jason_yao@htc.com>
2014-05-03 02:37:03 +0800

    update

---
+++ tool/巧用Graphviz和pvtrace等工具可視化C函數調用/test.c
@@ -0,0 +1,26 @@
+#include  <stdio.h>
+#include  <stdlib.h>
+
+void test1 ()
+{
+    printf ( "in test1.\n" );
+}
+void test2 ()
+{
+    test1 ();
+    printf ( "in test2.\n" );
+}
+void test3 ()
+{
+    test1 ();
+    test2 ();
+    printf ( "in test3.\n" );
+}
+int main ( int argc ,  char  * argv [])
+{
+    printf ( "Hello wolrd.\n" );
+    test1 ();
+    test2 ();
+    test3 ();
+    return  0 ;
+}
