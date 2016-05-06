d6ef8382d222798121901e51113fb68da08b0002
gigi <gigi>
2016-05-07 03:13:30 +0800

    update

---
+++ C/data/常用算法程序集_C_CPP_by_徐士良/ch9/9hmgs0.c
@@ -0,0 +1,19 @@
+
+#include "stdio.h"
+#include "9hmgs.c"
+main()
+{
+    double hmgsf(double);
+    printf("\n");
+    printf("g=%13.5e\n", hmgs(hmgsf));
+    printf("\n");
+}
+
+double hmgsf(x)
+double x;
+{
+    double y;
+    y = x * x * exp(-x * x);
+    return (y);
+}
+
